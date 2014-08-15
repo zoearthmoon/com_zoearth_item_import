cf479fe1970b8f8278747b954bda9864f5ebf2bd
zoearthmoon@gmail.com <zoearthmoon@gmail.com>
2014-08-15 12:14:52 +0800

    初步 讀取檔案 剩下    1.修改檔案內容的連結    2.修改檔案名稱    3.顯示UI...

--- administrator/components/com_zoearth_item_import/controllers/Import.php
+++ administrator/components/com_zoearth_item_import/controllers/Import.php
@@ -21,9 +21,106 @@ class ZoearthItemImportControllerImport extends ZoeController
         //20140424 zoearth 設定模板
         $view = $this->getDisplay(CONTROLLER.'/import');
         $this->getOptions(); //20130729 zoearth 選單資料
-        
+        $this->setupParams(); //20140425 zoearth 搜尋欄位
         $option = array();
+        
+        //20140815 zoearth 驗證與上傳
+        if ($this->isPost())
+        {
+            $imgUploadPath = JRequest::getVar('imgUploadPath');
+            $imgPrefix     = JRequest::getVar('imgPrefix');
+            $evernoteFile  = JRequest::getVar('evernoteFile','','files');
+            
+            if (!($evernoteFile['size'] > 0 && $imgPrefix && $evernoteFile ))
+            {
+                JError::raiseError(500,'Input ERROR 001');return FALSE;
+            }
+            if (!(substr($imgUploadPath,0,6) == 'images' && is_dir(JPATH_ROOT.DS.$imgUploadPath)))
+            {
+                JError::raiseError(500,'Input ERROR 002');return FALSE;
+            }
+            if (!($imgPrefix != '' && preg_match('/(^[a-zA-Z0-9\_\-]*)$/',$imgPrefix)))
+            {
+                JError::raiseError(500,'Input ERROR 003');return FALSE;
+            }
+            if (JFile::exists(JPATH_ROOT.DS.$imgUploadPath.DS.$imgPrefix.'_01.jpg'))
+            {
+                JError::raiseError(500,'Input ERROR 004');return FALSE;
+            }
+            
+            //20140815 zoearth 開始上傳到cache
+            $tmpZipDir = time();
+            $tmpZipName = $tmpZipDir.'.zip';
+            $tmpDir     = JPATH_ROOT.DS.'cache'.DS.'com_zoearth_item_import';
+            @mkdir(JPATH_ROOT.DS.'cache'.DS.'com_zoearth_item_import');
+            @copy($evernoteFile['tmp_name'],$tmpDir.DS.$tmpZipName);
+            $goZipFile = $tmpDir.DS.$tmpZipName;
 
+            $zip = new ZipArchive;
+            $res = $zip->open($goZipFile);
+            if (!($res === TRUE))
+            {
+                JError::raiseError(500,'Input ERROR 005 : Zip File Error ');return FALSE;
+            }
+            
+            @mkdir($tmpDir.DS.$tmpZipDir);
+            $zip->extractTo($tmpDir.DS.$tmpZipDir);
+            $zip->close();
+            //讀取資料夾
+            if (!(is_dir($tmpDir.DS.$tmpZipDir)))
+            {
+                JError::raiseError(500,'Input ERROR 006');return FALSE;
+            }
+            
+            if (!($dh = opendir($tmpDir.DS.$tmpZipDir)))
+            {
+                JError::raiseError(500,'Input ERROR 007');return FALSE;
+            }
+            
+            $goHtml = '';
+            $goImgDir = '';
+            while (($file = readdir($dh)) !== false)
+            {
+                $fType = filetype($tmpDir.DS.$tmpZipDir.DS.$file);
+                if ($fType == 'dir' && !in_array($file,array('.','..')))
+                {
+                    $goImgDir = $tmpDir.DS.$tmpZipDir.DS.$file;
+                }
+                if ($fType == 'file' && substr($file,-4,4) == 'html' )
+                {
+                    $goHtml = $tmpDir.DS.$tmpZipDir.DS.$file;
+                }
+            }
+            closedir($dh);
+            
+            if (!($goHtml != ''))
+            {
+                JError::raiseError(500,'Input ERROR 008');return FALSE;
+            }
+            
+            //20140815 zoearth 開始分析文字內容
+            $fileContent = file_get_contents($goHtml);
+            $fileContent = str_replace("\r",'',$fileContent);
+            $fileContent = str_replace("\n",'',$fileContent);
+            
+            //echo $fileContent;
+            //取出boby內容
+            preg_match("/<body([^>]*)>(.*)<\/body>/",$fileContent,$matches);
+            //去掉<a name="9303"/>
+            $fileContent = preg_replace('/<a([^>]*)name="([0-9]*)"([^>]*)\/>/','', $matches[2]);
+            
+            //if ($goImgDir)
+            
+            
+            
+            
+            
+            
+            
+            
+            exit();
+        }
+        
         $view->assignRef('data', $this->viewData);
         $view->display();
     }
@@ -51,11 +148,11 @@ class ZoearthItemImportControllerImport extends ZoeController
         if ($this->isPost() && $imgUploadPath)
         {
             //資料夾錯誤
-            if (substr($imgUploadPath,0,6) == 'images' && is_dir(JPATH_ROOT.$imgUploadPath))
+            if (substr($imgUploadPath,0,6) == 'images' && is_dir(JPATH_ROOT.DS.$imgUploadPath))
             {
-                if ($imgPrefix != '' && !preg_match("/^[a-zA-Z0-9\_\-]+$/",$imgPrefix))
+                if ($imgPrefix != '' && preg_match('/(^[a-zA-Z0-9\_\-]*)$/',$imgPrefix))
                 {
-                    if (!JFile::exists(JPATH_ROOT.$imgUploadPath.DS.$imgPrefix.'_01.jpg'))
+                    if (!JFile::exists(JPATH_ROOT.DS.$imgUploadPath.DS.$imgPrefix.'_01.jpg'))
                     {
                         //通過驗證
                         echo json_encode(array('result'=>1));exit();
@@ -67,7 +164,7 @@ class ZoearthItemImportControllerImport extends ZoeController
                 }
                 else
                 {
-                    echo json_encode(array('result'=>JText::_('COM_ZOEARTH_ITEM_IMPORT_IMG_PREFIX_ERROR_PREG')));exit();
+                    echo json_encode(array('result'=>JText::_('COM_ZOEARTH_ITEM_IMPORT_IMG_PREFIX_ERROR_PREG')." ".$imgPrefix));exit();
                 }
             }
             else
